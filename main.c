af42d730685a7f0d16cb4a666c000a55d6e7adfe
joaofabiopiresparra <joaofabiopiresparra@gmail.com>
2026-08-06 11:32:37 -0300

    Aula 1

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	
+	float a,b,c;
+	
+	a = 8;
+	b = 19;
+	c = a/b;
+	
+	printf ("a divisao de %f / %f = %f", a,b,c);
+	
+	return 0;
+}
