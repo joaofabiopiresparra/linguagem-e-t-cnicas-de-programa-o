7374f5dbcbb53b250a2647e74d63b74d2835154f
joaofabiopiresparra <joaofabiopiresparra@gmail.com>
2026-08-12 11:37:44 -0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#define pi 3.14
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
 	
-	float a,b,c;
+	float area, BASE, base, altura;
+	printf ("insira o valor da base maior, base menor e altura do trapesio: ");
+	scanf ("%f", &BASE);
+	scanf ("%f", &base);
+	scanf ("%f", &altura);	
+	area = ((BASE+base)*altura)/2;
 	
-	a = 8;
-	b = 19;
-	c = a/b;
-	
-	printf ("a divisao de %f / %f = %f", a,b,c);
-	
+	printf ("a area do trapesio e %f", area);
+
 	return 0;
 }
