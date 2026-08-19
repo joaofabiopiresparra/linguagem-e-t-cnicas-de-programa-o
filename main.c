adcd44e49fe6c1c8528241689e6e9c0fa8e055c3
joaofabiopiresparra <joaofabiopiresparra@gmail.com>
2026-08-19 11:02:22 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,18 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define pi 3.14
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
 	
-	float area, BASE, base, altura;
-	printf ("insira o valor da base maior, base menor e altura do trapesio: ");
-	scanf ("%f", &BASE);
-	scanf ("%f", &base);
-	scanf ("%f", &altura);	
-	area = ((BASE+base)*altura)/2;
-	
-	printf ("a area do trapesio e %f", area);
+	int a,b;
+	printf("escreva dois numeros: ")
+	scanf ("%d", &a)
+	scanf ("%d", &b)
+	printf ("%d%d", b, a);
 
 	return 0;
 }
