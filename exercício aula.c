20ef119c3c8f80f58e3dbd8fd619a6b446d5b608
joaofabiopiresparra <joaofabiopiresparra@gmail.com>
2026-08-26 11:21:29 -0300

    Update exercício aula.c

--- exercício aula.c
+++ exercício aula.c
@@ -1,1 +1,18 @@
-
+#include <stdio.h>
+#include <stdlib.h>
+int main(int argc, char *argv[]) {
+	int a, b, c, r;
+	printf("insira 3 valores: ");
+	scanf("%d %d %d", &a, &b, &c);
+	if(a>b){
+		r=a;
+	}else{
+		r=b;
+	}
+	if(r<c){
+		r=c;
+	}
+	printf("%d\n",r);
+	if(r%2==0) printf("o numero e par"); else printf("o numero e impar");
+	return 0;
+}
